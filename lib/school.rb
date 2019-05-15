3c4dbf52ab57602fc35732af1bc14eac150db79c
Emily Bolten <emilybolten@gmail.com>
2019-05-15 12:53:38 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require 'pry'
+
+class School
+
+    ROSTER = {}
+
+    def initialize(name)
+        @name = name
+    end
+
+    def roster=(roster)
+        @roster = roster 
+    end
+
+    def roster
+        ROSTER
+    end
+
+    def add_student(student_name, age)
+
+        if ROSTER[age] == nil
+            ROSTER[age] = []
+        end
+        
+        ROSTER[age] << student_name
+    end
+
+    def grade(grade)
+        ROSTER[grade]
+    end
+
+    def sort
+        ROSTER.sort.to_h
+    end
+end
